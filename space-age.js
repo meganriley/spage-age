be83473a427990f8d4474d23a89f630306317071
Meg <meganriley806@gmail.com>
2022-11-04 05:49:28 -0500

    all tests passing

---
+++ space-age.js
@@ -0,0 +1,21 @@
+//
+// This is only a SKELETON file for the 'Space Age' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const age = (name, ageInSeconds) => {
+  let earthYear = 31557600;
+
+  const planets = {
+    "mercury": 0.2408467,
+    "venus": 0.61519726,
+    "earth": 1,
+    "mars": 1.8808158,
+    "jupiter": 11.862615,
+    "saturn": 29.447498,
+    "uranus": 84.016846,
+    "neptune": 164.79132
+  }
+  let spaceAge = earthYear * planets[name]
+  return parseFloat((ageInSeconds / spaceAge).toFixed(2));
+};
